a654fc1519c99ee3fc654266c4e42ccefb050d3f
Daniel Rinser <github@danielrinser.de>
2009-12-16 13:48:58 +0100

    Profiled DSL in controllers now allows specification of "includeView" boolean option. If set to true, view rendering will be included in profiling. Default is false.

--- grails-app/conf/Perf4jFilters.groovy
+++ grails-app/conf/Perf4jFilters.groovy
@@ -11,6 +11,8 @@ public class Perf4jFilters {
     static final String PROFILED_PROPERTY = "profiled"
     // the key used to store the stopwatch object in the request
     static final String STOPWATCH_REQUEST_KEY = 'perf4jplugin.stopwatch'
+    // the key used to store the includeView flag in the request
+    static final String INCLUDE_VIEW_REQUEST_KEY = 'perf4jplugin.includeView'
     
     // profiling options for each controller which has a "profiled" property of type Closure (controller name is map key)
     // this is needed to cache the options, so we don't have to execute the closure upon each request
@@ -35,11 +37,11 @@ public class Perf4jFilters {
 
                         if(profiled instanceof Boolean && profiled) {
                             log.trace "Boolean type profiled property in ${controller}"
-                            createStopwatch(null, null, controller, action, request)
+                            createStopwatch(null, null, false, controller, action, request)
                         }
                         else if(profiled instanceof List && profiled.contains(action)) {
                             log.trace "Collection type profiled property in ${controller}"
-                            createStopwatch(null, null, controller, action, request)
+                            createStopwatch(null, null, false, controller, action, request)
                         }
                         else if(profiled instanceof Closure) {
                             log.trace "Closure type profiled property in ${controller}"
@@ -60,7 +62,7 @@ public class Perf4jFilters {
 
                             def options = profilingOptions[controllerName]
                             if(options && options.containsKey(action)) {
-                                createStopwatch(options[action].tag, options[action].message, controller, action, request)
+                                createStopwatch(options[action].tag, options[action].message, options[action].includeView as Boolean, controller, action, request)
                             }
                         }
                     }
@@ -69,16 +71,20 @@ public class Perf4jFilters {
 
             
             after = {
-                def stopwatch = request[STOPWATCH_REQUEST_KEY]
+                def includeView = request[INCLUDE_VIEW_REQUEST_KEY]
             
-                if(stopwatch) {
-                    stopwatch.stop()
-                    log.trace "Stop watch stopped: ${stopwatch.tag}"
+                if(!includeView) {
+                    stopStopwatch(request)
                 }
             }
 
 
             afterView = {
+                def includeView = request[INCLUDE_VIEW_REQUEST_KEY]
+            
+                if(includeView) {
+                    stopStopwatch(request)
+                }
             }
         }
     }
@@ -87,7 +93,7 @@ public class Perf4jFilters {
     /**
      *  Create the stop watch and store it in the request, so it can be accessed in the after/afterView interceptors.
      */
-    private static createStopwatch(tag, message, controllerName, actionName, request) {
+    private static createStopwatch(String tag, String message, Boolean includeView, String controllerName, String actionName, request) {
         if(!tag) {
             tag = "${controllerName}.${actionName}"
         }
@@ -95,6 +101,20 @@ public class Perf4jFilters {
         def stopwatch = new Log4JStopWatch(tag, message)
 
         request[STOPWATCH_REQUEST_KEY] = stopwatch
+        request[INCLUDE_VIEW_REQUEST_KEY] = includeView
+        
         log.trace "Stop watch started: ${tag}"
+    }
+    
+    
+    /**
+     *  Stop the stopwatch (if there is one).
+     */
+    private static stopStopwatch(request) {
+        def stopwatch = request[STOPWATCH_REQUEST_KEY]
+        if(stopwatch) {
+            stopwatch.stop()
+            log.trace "Stop watch stopped: ${stopwatch.tag}"
+        }
     }
 }
